530325f780b2e28057ccdb81ecf4a50b0a16ba49
Takiguchii <alexandre.pupim360@gmail.com>
2026-01-19 21:04:49 -0300

    Feat: Criando camuflagem utilizando user-agent

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-import requests
+import requests 
+from bs4 import BeautifulSoup
 
 url = 'http://quotes.toscrape.com'
-
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win 64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
+    }
 print(f"Tentando conectar em: {url}")
 
-resposta = requests.get(url)
-
-print(f"O código da resposta foi: {resposta.status_code}")
+resposta = requests.get(url, headers=headers)
 
 if resposta.status_code == 200:
     print("Sucesso! A porta está aberta.")
-else:
-    print("Erro! Algo deu errado.")
