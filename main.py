f3d9cd4c1b8ff609de50359cf7ff08e2cb92d2c1
Takiguchii <alexandre.pupim360@gmail.com>
2026-01-20 21:38:30 -0300

    Refactor: refatorando codigo para raspar todos os 'titulos' e 'preços' do html

--- main.py
+++ main.py
@@ -12,12 +12,16 @@ resposta = requests.get(url, headers=headers)
 if resposta.status_code == 200:
     print("Saborr porta está aberta.")
     soup = BeautifulSoup(resposta.text, 'html.parser')
-
     livros = soup.find_all('article', class_='product_pod')
-    primeiro_livro = livros[0]
-    tag_titulo = primeiro_livro.find('h3').find('a')
-    titulo = tag_titulo['title']
 
-    print(f"Saborr titulo do livro:{titulo}")
+    for livro in livros:
+        tag_titulo = livro.find('h3').find('a')
+        titulo = tag_titulo['title']
+        print(f"Saborr titulo do livro:{titulo}")
+
+        tag_preco = livro.find('p', class_='price_color') # Pegando preço no elemento html
+        preco = tag_preco.text
+        print(f"Saborr preço do livro: {preco}")
+
 else:
     print("SABOR deu ruim o scraping....")
